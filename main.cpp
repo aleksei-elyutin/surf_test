d2c148efd52af3dae6396a61dcadfd02b4911ece
Aleksei Elyutin <aleksei_elyutin@mail.ru>
2017-02-02 16:33:15 +0300

    detecting feature points on image

--- main.cpp
+++ main.cpp
@@ -1,10 +1,41 @@
 #include <iostream>
+#include <opencv2/features2d.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/xfeatures2d.hpp>
+#include <opencv2/core.hpp>
+#include "opencv2/imgproc.hpp"
+
+#define IMAGE_ "C:/opencv-master/opencv/samples/data/aero1.jpg"
 
 using namespace std;
+using namespace cv;
+using namespace cv::xfeatures2d;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    Mat input_image, copy;
+    input_image = imread(IMAGE_, CV_LOAD_IMAGE_COLOR);
+    copy = input_image.clone();
+
+
+    double hessian_threshold = 2000;
+    Ptr<Feature2D> surf_detector_obj;
+    vector<KeyPoint> keypoints1;
+
+    surf_detector_obj = SURF::create(hessian_threshold);
+    surf_detector_obj->detect( input_image, keypoints1);
+    for( size_t i = 0; i < keypoints1.size(); i++ )
+    {
+        KeyPoint current_kp;
+        Point2f kp_coords;
+        current_kp = keypoints1[i];
+        kp_coords = current_kp.pt;
+        circle( copy, kp_coords , 4, Scalar(0, 0, 255), 1, 8, 0 );
+    }
+
+    namedWindow( "SURF result", WINDOW_AUTOSIZE );
+    imshow( "SURF result", copy );
+    waitKey();
     return 0;
 }
 
--- surf_test.pro
+++ surf_test.pro
@@ -5,3 +5,14 @@ CONFIG -= qt
 
 SOURCES += main.cpp
 
+OPENCV_LIB_PATH = C:\opencv-build\install\x86\mingw\bin
+
+INCLUDEPATH += C:\opencv-build\install\include
+
+
+CONFIG(debug) {
+    include(opencv_debug.pri)
+}
+CONFIG(release) {
+    include(opencv_release.pri)
+}
